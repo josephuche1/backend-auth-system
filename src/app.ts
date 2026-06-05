d0b46725391935d02b8bd4bc07cf0dfd4f261c07
Uche Joseph <josephuche2404@gmail.com>
2026-06-05 20:49:03 +0300

    User registration works now

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./modules/auth/auth.routes";
 
 dotenv.config();
 
@@ -8,6 +9,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.get("/auth/register", (req, res) => {
+  res.json({ message: "Register endpoint" });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend API running 🚀" });
