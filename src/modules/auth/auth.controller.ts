189e6ed967912b99c832c5d5fc435999a082978b
Uche Joseph <josephuche2404@gmail.com>
2026-06-05 20:30:33 +0300

    Added routes and controllers to hlp with login and registering of users

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { registerUser, loginUser } from "./auth.service";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const result = await registerUser(username, email, password);
+
+    res.status(201).json(result);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const result = await loginUser(email, password);
+
+    res.status(200).json(result);
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
