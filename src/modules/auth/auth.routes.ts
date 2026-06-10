e42a9141ca303786448a7838ce10f9a56914530f
Uche Joseph <josephuche2404@gmail.com>
2026-06-10 07:17:52 +0300

    Add input validation using Zod

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { register, login, refresh, logout } from "./auth.controller";
+import { registerSchema, loginSchema } from "../../validation/auth.validation";
+import { validate } from "../../middleware/validate.middleware";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register",validate(registerSchema),  register);
+router.post("/login",validate(loginSchema), login);
 router.post("/refresh", refresh);
 router.post("/logout", logout);
 
